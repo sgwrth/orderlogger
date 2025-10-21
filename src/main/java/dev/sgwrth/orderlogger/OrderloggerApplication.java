eeec1c59becd15d348b85d0aba34357e8c105e0b
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-10-21 20:22:26 +0200

    initial commit

---
+++ src/main/java/dev/sgwrth/orderlogger/OrderloggerApplication.java
@@ -0,0 +1,13 @@
+package dev.sgwrth.orderlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderloggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sgwrth/orderlogger/OrderloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sgwrth.orderlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
