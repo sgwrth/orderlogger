599a312cbee09d985beccc122bc46c4591be9a0e
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-10-23 20:01:25 +0200

    * add: application.yaml with Postgres connection config * add: basic Orders entity * change: renamed Postgres db to 'orderlogger'

---
+++ src/main/java/dev/sgwrth/orderlogger/controller/TestController.java
@@ -0,0 +1,13 @@
+package dev.sgwrth.orderlogger.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/")
+	public String getTest() {
+		return "success!";
+	}
+}
