724008716c04d9abd0ec94e1b3494bf1c6a5377a
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-11-03 11:24:39 +0100

    * add: Spring Security * add: SecurityConfig class

---
+++ src/main/java/dev/sgwrth/orderlogger/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package dev.sgwrth.orderlogger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(
+					authorizeHttpRequests -> {
+						authorizeHttpRequests
+							.anyRequest().authenticated();
+					}
+			)
+			.formLogin(Customizer.withDefaults())
+			.httpBasic(Customizer.withDefaults());
+		return http.build();
+	}
+
+}
